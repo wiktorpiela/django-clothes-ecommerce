a51702fde29669e5324d33d6600eec0e6d76492e
Wiktor <wpiela9@gmail.com>
2023-05-22 12:46:22 +0200

    custom user model, category model, media files config

--- djangoclothes/settings.py
+++ djangoclothes/settings.py
@@ -27,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "category",
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -59,6 +61,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djangoclothes.wsgi.application'
 
+AUTH_USER_MODEL = "accounts.Account"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -69,7 +72,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -112,6 +114,10 @@ STATIC_ROOT = BASE_DIR / "static"
 STATICFILES_DIRS = [
     "djangoclothes/static",
 ]
+
+#media set up
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR/"media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
