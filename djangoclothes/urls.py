028e7295daee2697f05acecf05f7cbe16ed54bc2
Wiktor <wpiela9@gmail.com>
2023-05-23 11:50:08 +0200

    cart and cartitem models added

--- djangoclothes/urls.py
+++ djangoclothes/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("cart/", include("carts.urls")),
     path("", include("store.urls")),
 ]
 
