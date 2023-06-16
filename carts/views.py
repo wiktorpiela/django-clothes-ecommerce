21e4c0041e246b55ecaf08e1a5a8c7138f5a64d7
Wiktor <wpiela9@gmail.com>
2023-06-16 07:24:01 +0200

    grouping cart item variations

--- carts/views.py
+++ carts/views.py
@@ -56,17 +56,34 @@ def add_cart(request, productID):
 
     cart.save()
 
-    try:
-        cart_item = CartItem.objects.get(product=product, cart=cart)
+    cartItemExists = CartItem.objects.filter(product=product, cart=cart).exists()
 
-        if len(product_variation) > 0:
-            cart_item.variations.clear()
-            for item in product_variation:
-                cart_item.variations.add(item)
+    if cartItemExists:
+        cart_item = CartItem.objects.filter(product=product, cart=cart)
+        ex_var_list = []
+        ids = []
+        for item in cart_item:
+            existing_variation = item.variations.all()
+            ex_var_list.append(list(existing_variation))
+            ids.append(item.id)
 
-        cart_item.quantity += 1
-        cart_item.save()
-    except CartItem.DoesNotExist:
+        if product_variation in ex_var_list:
+            #increase cart item quantity
+            index = ex_var_list.index(product_variation)
+            itemId = ids[index]
+            item = CartItem.objects.get(product=product, id=itemId)
+            item.quantity += 1
+            item.save()
+
+        else:
+            #create new cart item
+            item = CartItem.objects.create(product=product, quantity = 1, cart=cart)
+            if len(product_variation) > 0:
+                item.variations.clear()
+                item.variations.add(*product_variation)
+            item.save()
+
+    else:
         cart_item = CartItem.objects.create(
             product = product,
             quantity = 1,
@@ -75,9 +92,7 @@ def add_cart(request, productID):
         
         if len(product_variation) > 0:
             cart_item.variations.clear()
-            for item in product_variation:
-                cart_item.variations.add(item)
-
+            cart_item.variations.add(*product_variation)
         cart_item.save()
         
     return redirect("carts:cart")
