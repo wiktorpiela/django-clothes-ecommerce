a7bf7371454b876d98be8a28991d6d3764e521b8
Wiktor <wpiela9@gmail.com>
2023-06-19 13:46:45 +0200

    checkout page

--- carts/urls.py
+++ carts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.cart, name="cart"),
     path("add-cart/<int:productID>/", views.add_cart, name="add_cart"),
     path("remove-cart/<int:productID>/<str:actionLocation>/<int:cartItemID>/", views.remove_cart, name="remove_cart"),
+    path("checkout/", views.checkout, name="checkout"),
     
 ]
--- carts/views.py
+++ carts/views.py
@@ -114,9 +114,29 @@ def remove_cart(request, productID, actionLocation, cartItemID):
     else:
         cart_item.delete()
 
-
-
     return redirect("carts:cart")
+
+def checkout(request, total=0, quantity=0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id = _cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += cart_item.product.price * cart_item.quantity
+            quantity += cart_item.quantity
+        tax = 23*total/100
+        grand_total = total+tax
+    except CartItem.DoesNotExist:
+        pass
+
+    context = {
+        "total":total,
+        "quantity":quantity,
+        "cart_items":cart_items,
+        "tax":tax,
+        "grand_total":grand_total
+        }
+
+    return render(request, "store/checkout.html", context)
 
 
 
