58173c16781a2164e678f9d636bcb3ce90b92e16
Wiktor <wpiela9@gmail.com>
2023-06-22 06:47:51 +0200

    payment details to backend

--- orders/views.py
+++ orders/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from carts.models import CartItem
 from .forms import OrderForm
-from .models import Order
+from .models import Order, Payment
 from datetime import datetime
+import json
 
 def place_order(request, total=0, quantity=0):
     cart_items = CartItem.objects.filter(user=request.user)
@@ -19,7 +20,7 @@ def place_order(request, total=0, quantity=0):
         total += cart_item.product.price * cart_item.quantity
         quantity += cart_item.quantity
 
-    tax = 23*total/100
+    tax = round(23*total/100,2)
     grand_total = total+tax
     
     if request.method == "POST":
@@ -65,6 +66,21 @@ def place_order(request, total=0, quantity=0):
         return redirect("carts:checkout")
     
 def payments(request):
+    body = json.loads(request.body)
+    order = Order.objects.get(user=request.user, is_ordered=False, order_number=body["orderID"])
+    #Store trans details in paymnts model
+    payment = Payment(
+        user = request.user,
+        payment_id = body["transID"],
+        payment_method = body["payment_method"],
+        amount_paid = order.order_total,
+        status = body["status"],
+    )
+    payment.save()
+    order.is_ordered = True
+    order.payment = payment
+    order.save()
+
     return render(request, "orders/payments.html")
 
         
