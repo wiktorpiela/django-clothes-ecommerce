145795a5d66e4e9517a3703374afd208fe7bcfda
Wiktor <wpiela9@gmail.com>
2023-06-29 22:06:41 +0200

    predeploy preparation

--- djangoclothes/settings.py
+++ djangoclothes/settings.py
@@ -2,6 +2,8 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 from django.contrib.messages import constants as messages
+import cloudinary
+import cloudinary_storage
 
 load_dotenv()
 
@@ -15,9 +17,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
+
+CSRF_TRUSTED_ORIGINS = ['djangoclothes-store.up.railway.app']
 
 # Application definition
 
@@ -33,6 +37,8 @@ INSTALLED_APPS = [
     "store",
     "carts",
     "orders",
+    "cloudinary_storage",
+    "cloudinary",
 ]
 
 MIDDLEWARE = [
@@ -43,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'djangoclothes.urls'
@@ -72,12 +79,33 @@ AUTH_USER_MODEL = "accounts.Account"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+if DEBUG == False:#use postgresql in production
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ["PGDATABASE"],
+            'USER': os.environ["PGUSER"],
+            'PASSWORD': os.environ["PGPASSWORD"],
+            'HOST': os.environ["PGHOST"],
+            'PORT': os.environ["PGPORT"],
+        }
+    }
+
+    CLOUDINARY_STORAGE = {
+        'CLOUD_NAME': os.environ["CLOUD_NAME"],
+		'API_KEY': os.environ["API_KEY"],
+		'API_SECRET': os.environ["API_SECRET"]
+		}
+    
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+else:
+	DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+	
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- static/js/script.js
+++ static/js/script.js
@@ -58,4 +58,6 @@ $(document).ready(function() {
 }); 
 // jquery end
 
-
+setTimeout(function(){
+    $('#message').fadeOut('slow')
+}, 4000)
